4c8867ef5a471bd1c29b7701c28506e9b87a0b84
K M Marziul Karim <marziulrafi@gmail.com>
2025-04-16 21:28:19 +0600

    new pricing cards

--- src/components/options/Pricing.jsx
+++ src/components/options/Pricing.jsx
@@ -1,5 +1,6 @@
 import React, {use} from 'react';
 import PricingCard from '../cards/PricingCard';
+import DaisyPricing from '../daisy_pricing/DaisyPricing';
 
 const Pricing = ({pricingPromise}) => {
     const pricingData = use(pricingPromise);
@@ -8,9 +9,13 @@ const Pricing = ({pricingPromise}) => {
         <div>
             <h2 className='text-5xl mt-8 mb-8'>Get Our Membership</h2>
 
-            <div className='grid grid-cols-3 gap-8'>
-                {
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                {/* {
                     pricingData.map(pricing=><PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+                } */}
+
+                {
+                    pricingData.map(pricing=><DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>)
                 }
             </div>
         </div>
